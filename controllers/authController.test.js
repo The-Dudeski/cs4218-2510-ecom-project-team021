3621c431a3e9c5d6e69ff351ab8cc29c8bfdf7b5
HusseinSafwan02 <safwanhussein.ri@gmail.com>
2025-10-02 04:39:22 +0800

    Finalised tests for loginController

--- controllers/authController.test.js
+++ controllers/authController.test.js
@@ -1,9 +1,14 @@
-import { registerController } from "./authController.js"
+import { registerController, loginController } from "./authController.js"
 import userModel from "../models/userModel.js";
-import { hashPassword } from "../helpers/authHelper.js";
+import { hashPassword, comparePassword } from "../helpers/authHelper.js";
+import JWT from "jsonwebtoken";
+import { beforeEach } from "node:test";
 
 jest.mock("../models/userModel.js")
 jest.mock("../helpers/authHelper.js")
+jest.mock("jsonwebtoken", () => ({
+  sign: jest.fn(),
+}))
 
 const mockResponse = () => {
   const res = {};
@@ -226,3 +231,150 @@ describe("registerController", () => {
   });
 
 });
+
+
+describe("loginController", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    process.env.JWT_SECRET = "testsecret";
+  });
+
+  it("email or password missing", async () => {
+    // Arrange
+    const req = { body: {
+      email: "",
+      password: ""
+    }};
+    const res = mockResponse();
+
+    // Act
+    await loginController(req, res);
+
+    // Assert
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.send).toHaveBeenCalledWith({
+      success: false,
+      message: "Invalid email or password",
+    });
+
+  });
+
+  it("user not found", async () => {
+    // Arrange
+    const req = { body: {
+      email: "safwan@gmail.com",
+      password: "password"
+    }};
+    const res = mockResponse();
+    userModel.findOne.mockResolvedValueOnce(null);
+
+    // Act
+    await loginController(req, res);
+
+    // Assert
+    expect(userModel.findOne).toHaveBeenCalledWith({email: "safwan@gmail.com"});
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.send).toHaveBeenCalledWith({
+      success: false,
+      message: "Email is not registered",
+    });
+  });
+
+  it("wrong password with 200 error", async () => {
+    // Arrange
+    const req = { body: {
+      email: "safwan@gmail.com",
+      password: "wrong"
+    }};
+    const res = mockResponse();
+    const fakeUser = {
+      _id: "userid",
+      email: "safwan@gmail.com",
+      password: "correct"
+    };
+
+    userModel.findOne.mockResolvedValueOnce(fakeUser);
+    comparePassword.mockResolvedValueOnce(false);
+
+    // Act
+    await loginController(req, res);
+
+    // Assert
+    expect(comparePassword).toHaveBeenCalledWith("wrong", "correct");
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.send).toHaveBeenCalledWith({
+      success: false,
+      message: "Invalid Password",
+    });
+  });
+
+  it("exception caught due to error caught in catch block", async () => {
+    // Arrange
+    const req = { body: {
+      email: "safwan@gmail.com",
+      password: "password"
+    }};
+    const res = mockResponse();
+
+    const fakeErr = new Error("Fake error");
+    userModel.findOne.mockRejectedValueOnce(fakeErr);
+    
+    // Act
+    await loginController(req, res);
+    // Assert
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.send).toHaveBeenCalledWith({
+      success: false,
+      message: "Error in login",
+      error: fakeErr
+    });
+  });
+
+  it("log in user and return token and payload", async () => {
+    // Arrange
+    const req = { body: {
+      email: "safwan@gmail.com",
+      password: "correct"
+    }};
+    const res = mockResponse();
+    const fakeUser = {
+      _id: "userid",
+      name: "Safwan",
+      email: "safwan@gmail.com",
+      phone: "123456",
+      address: "NUS",
+      role: 0,
+      password: "hashedcorrect"
+    };
+
+    userModel.findOne.mockResolvedValueOnce(fakeUser);
+    comparePassword.mockResolvedValueOnce(true);
+    JWT.sign.mockReturnValueOnce("faketoken");
+
+    // Act
+    await loginController(req, res);
+
+    // Assert
+    expect(JWT.sign).toHaveBeenCalledWith(
+      { _id: "userid" }, 
+      process.env.JWT_SECRET, 
+      { expiresIn: "7d" }
+    );
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.send).toHaveBeenCalledWith({
+      success: true,
+      message: "login successfully",
+      user: {
+        _id: "userid",
+        name: "Safwan",
+        email: "safwan@gmail.com",
+        phone: "123456",
+        address: "NUS",
+        role: 0,
+      },
+      token: "faketoken",
+    });
+  });
+});
+
+
