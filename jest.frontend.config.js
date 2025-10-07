7a1c21890464da98f2f33ac96f83ef1246149885
Lorenz Dude <lorenzdude@Lorenzs-MacBook-Air.local>
2025-10-07 17:39:21 +0800

    Add unit tests for Sprint 2

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -19,17 +19,33 @@ export default {
   // ignore all node_modules except styleMock (needed for css imports)
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
-  // only run these tests
-  // testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js"],
-  testMatch: ["<rootDir>/client/src/pages/CartPage.test.js"],
+  // discover all frontend tests under client/src
+  testMatch: ["<rootDir>/client/src/**/*.test.js"],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ['client/src/pages/CartPage.js'],
+  collectCoverageFrom: [
+    // Only include tested pages to target 100% page coverage
+    'client/src/pages/About.js',
+    'client/src/pages/CartPage.js',
+    'client/src/pages/Contact.js',
+    'client/src/pages/Policy.js',
+    'client/src/pages/Pagenotfound.js',
+    'client/src/pages/Categories.js',
+    'client/src/pages/CategoryProduct.js',
+    'client/src/pages/ProductDetails.js',
+    'client/src/pages/Search.js',
+    // Exclude generated content
+    '!client/src/_site/**',
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/client/src/_site/',
+  ],
   coverageThreshold: {
     global: {
       lines: 90,
-      functions: 85,
+      functions: 90,
     },
   },
   setupFilesAfterEnv: ["<rootDir>/client/src/setupTests.js"],
